364246c04cc295ff68e086f14a6f79557ec914d2
Cem Denizsel <45531291+cemdenizsel@users.noreply.github.com>
2019-12-16 00:11:29 +0300

    Arranged RoundView and Controller Classes

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,4 +1,21 @@
 package Controller;
 
 public class Controller {
+
+    private Game game_model;
+    private View game_view;
+
+
+    public Controller(Game game_model, View game_view) {
+        this.game_model = game_model;
+        this.game_view = game_view;
+    }
+
+    public View getGame_view() {
+        return game_view;
+    }
+
+    public void setGame_view(View game_view) {
+        this.game_view = game_view;
+    }
 }
--- src/GUI/RoundView.java
+++ src/GUI/RoundView.java
@@ -1,4 +1,33 @@
 package GUI;
 
+import javax.swing.*;
+
 public class RoundView extends View {
+
+    private JPanel panel;
+    private JPanel playerCardPanel;
+    private JPanel dealerCardPanel;
+    private JLabel dealerLabel;
+    private JLabel playerLabel;
+    private JButton hitButton;
+    private JButton stayButton;
+    private JButton pauseButton;
+    private JButton resumeButton;
+
+
+    public JLabel getDealerLabel() {
+        return dealerLabel;
+    }
+
+    public JLabel getPlayerLabel() {
+        return playerLabel;
+    }
+
+    public void setDealerLabel(JLabel dealerLabel) {
+        this.dealerLabel = dealerLabel;
+    }
+
+    public void setPlayerLabel(JLabel playerLabel) {
+        this.playerLabel = playerLabel;
+    }
 }
