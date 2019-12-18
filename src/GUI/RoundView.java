51ad15458f29a7b52f359b1975b5fe44e86d5949
Taci Ata Küçükpınar <taci.ata72@gmail.com>
2019-12-18 20:49:37 +0300

    Added buttons and labels to the RoundView with its constructor.

--- src/GUI/RoundView.java
+++ src/GUI/RoundView.java
@@ -1,6 +1,8 @@
 package GUI;
 
 import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
 
 public class RoundView extends View {
 
@@ -14,6 +16,42 @@ public class RoundView extends View {
     private JButton pauseButton;
     private JButton resumeButton;
 
+    JLabel playerCard1;
+    JLabel playerCard2;
+    JLabel playerCardHit;
+    JLabel dealerCard0;
+    JLabel dealerCard2;
+    JLabel dealerCard1;
+    JLabel dealerCardHit;
+
+    public RoundView () throws IOException {
+        super();
+
+        panel.setBackground(new Color(0, 122, 0));
+        dealerCardPanel.setBackground(new Color(0, 122, 0));
+        playerCardPanel.setBackground(new Color(0, 122, 0));
+
+        panel.setLayout(new FlowLayout());
+        hitButton.setText("  Hit");
+        hitButton.setEnabled(false);
+        stayButton.setText("  Stay");
+        stayButton.setEnabled(false);
+
+
+        dealerLabel.setText("  Dealer:  ");
+        playerLabel.setText("  Player:  ");
+
+        panel.add(hitButton);
+        panel.add(stayButton);
+        playerCardPanel.add(playerLabel);
+        dealerCardPanel.add(dealerLabel);
+
+        setLayout(new BorderLayout());
+        add(panel, BorderLayout.NORTH);
+        add(dealerCardPanel, BorderLayout.CENTER);
+        add(playerCardPanel, BorderLayout.SOUTH);
+
+    }
 
     public JLabel getDealerLabel() {
         return dealerLabel;
