923510507b0b860289937c81fc2f287295cd3e12
Cem Denizsel <45531291+cemdenizsel@users.noreply.github.com>
2019-12-19 21:06:42 +0300

    Added Validation JOptionPane.

--- src/GUI/StartView.java
+++ src/GUI/StartView.java
@@ -1,14 +1,23 @@
 package GUI;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class StartView extends JPanel {
 
-    StartView(){
-        super();
-        JButton jb = new JButton("Berker");
-        add(jb);
-        setVisible(true);
-    }
+   public StartView() {
 
-}
+       JDialog.setDefaultLookAndFeelDecorated(true);
+       int response = JOptionPane.showConfirmDialog(null, "Are you older than 18 years old?", "Validation",
+               JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+       if (response == JOptionPane.NO_OPTION) {
+           System.exit(0);
+       } else if (response == JOptionPane.YES_OPTION) {
+           System.out.println();
+       } else if (response == JOptionPane.CLOSED_OPTION) {
+           System.exit(0);
+       }
+   }
+   }
+
+
